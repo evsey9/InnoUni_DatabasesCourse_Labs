0c233818241a1a15489d6855612d26b54559291f
Evsey <evseyru1@mail.ru>
2023-04-04 16:10:01 +0300

    change base of week 10

--- week10/base.py
+++ week10/base.py
@@ -1,8 +1,9 @@
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 client = MongoClient("mongodb://localhost") # will connect to localhost
 db = client['test']
 
 
 posts = db.restaurants
 
-print(posts.find_one())
+print(posts.find_one({"_id": ObjectId('6427cf9a6091411bad4cc649')}))
